7b011f4cf617cd9f21bf1ac25c0cc8bb237d0191
Rafael Langoni <langoninaves@gmail.com>
2024-12-16 06:46:42 -0330

    adjusting import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,9 +3,9 @@ insert into kitchen (name, id) values ('Japanese', 2);
 insert into kitchen (name, id) values ('Chinese', 3);
 insert into kitchen (name, id) values ('Brazilian', 4);
 
-insert into restaurant (id, name, kitchen_id) values (1, 'Feijoadao do Ze', 4);
-insert into restaurant (id, name, kitchen_id) values (2, 'Sushi do Joao', 2);
-insert into restaurant (id, name, kitchen_id) values (3, 'Yakisoba do Mane', 3);
+insert into restaurant (id, name, delivery_fee, kitchen_id) values (1, 'Feijoadao do Ze', 4.99, 4);
+insert into restaurant (id, name, delivery_fee, kitchen_id) values (2, 'Sushi do Joao', 5.99,2);
+insert into restaurant (id, name, delivery_fee, kitchen_id) values (3, 'Yakisoba do Mane', 6.99,3);
 
 insert into payment_type (id, description) values (1, 'Credit card');
 insert into payment_type (id, description) values (2, 'Gift card');
