5ca1ebd1bb36464310e165320cda9374ba1a7574
Rafael Langoni <langoninaves@gmail.com>
2024-12-20 05:35:37 -0330

    personalizing Headers when necessary

--- src/main/java/com/langoni/eatzy_peasy/api/controller/KitchenController.java
+++ src/main/java/com/langoni/eatzy_peasy/api/controller/KitchenController.java
@@ -4,8 +4,10 @@ import com.langoni.eatzy_peasy.model.Kitchen;
 import com.langoni.eatzy_peasy.model.KitchenXmlWrapper;
 import com.langoni.eatzy_peasy.repository.implementation.KitchenRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,28 +20,42 @@ public class KitchenController {
     private KitchenRepositoryImpl kitchenRepository;
 
     @GetMapping
-    public List<Kitchen> listAll(){
+    public List<Kitchen> listAll() {
         return kitchenRepository.listAllKitchens();
     }
 
     @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public KitchenXmlWrapper listAllXml(){
+    public KitchenXmlWrapper listAllXml() {
         return new KitchenXmlWrapper(kitchenRepository.listAllKitchens());
     }
 
     @GetMapping(value = "/{kitchenId}")
-    public Kitchen findKitchenById(@PathVariable(value = "kitchenId") Long id){
-        return kitchenRepository.findKitchenById(id);
+    public ResponseEntity<Kitchen> findKitchenById(@PathVariable(value = "kitchenId") Long id) {
+        //Implementing customized headers, just an example
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/kitchens");
+//
+//        return ResponseEntity
+//                .status(HttpStatus.FOUND)
+//                .headers(headers)
+//                .build();
+        var kitchen = kitchenRepository.findKitchenById(id);
+
+        if (kitchen != null) {
+            return ResponseEntity.ok(kitchen);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
-    public void removeKitchen(@PathVariable Long id){
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void removeKitchen(@PathVariable Long id) {
         kitchenRepository.removeKitchen(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Kitchen addKitchen(@RequestBody Kitchen kitchen){
+    public Kitchen addKitchen(@RequestBody Kitchen kitchen) {
         return kitchenRepository.addKitchen(kitchen);
     }
 }
