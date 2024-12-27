49cf69ddd594e31c1a4eb8648607377b9fa375ae
Rafael Langoni <langoninaves@gmail.com>
2024-12-27 13:05:32 -0330

    dynamic query

--- src/main/java/com/langoni/eatzy_peasy/infra/repository/implementation/RestaurantRepositoryImpl.java
+++ src/main/java/com/langoni/eatzy_peasy/infra/repository/implementation/RestaurantRepositoryImpl.java
@@ -1,29 +1,59 @@
 package com.langoni.eatzy_peasy.infra.repository.implementation;
 
 import com.langoni.eatzy_peasy.domain.model.Restaurant;
+import com.langoni.eatzy_peasy.domain.repository.RestaurantRepositoryImplInterface;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
-public class RestaurantRepositoryImpl {
+public class RestaurantRepositoryImpl implements RestaurantRepositoryImplInterface {
 
     @PersistenceContext
     private EntityManager entityManager;
 
     public List<Restaurant> listAllRestaurantCustomizedInterface(String name, BigDecimal initialDeliveryFee, BigDecimal finalDeliveryFee) {
 
-        var jpql = "select r from Restaurant r where r.name like :name " +
-                "and r.deliveryFee between :initialDeliveryFee and :finalDeliveryFee ";
+//        var jpql = "select r from Restaurant r where r.name like :name " +
+//                "and r.deliveryFee between :initialDeliveryFee and :finalDeliveryFee ";
 
-        return entityManager.createQuery(jpql, Restaurant.class)
-                .setParameter("name", "%" + name + "%")
-                .setParameter("initialDeliveryFee", initialDeliveryFee)
-                .setParameter("finalDeliveryFee", finalDeliveryFee)
-                .getResultList();
+        StringBuilder jpql = new StringBuilder();
+        jpql.append("select r from Restaurant r where 0=0 ");
+
+        var parameters = new HashMap<String, Object>();
+
+        if(StringUtils.hasLength(name)){
+            jpql.append(" and r.name like :name ");
+            parameters.put("name", "%" + name + "%");
+        }
+
+        if(initialDeliveryFee != null){
+            jpql.append(" and r.deliveryFee >= :initialFee");
+            parameters.put("initialFee", initialDeliveryFee);
+        }
+
+        if(finalDeliveryFee != null){
+            jpql.append(" and r.deliveryFee <= :finalFee ");
+            parameters.put("finalFee", finalDeliveryFee);
+        }
+
+        TypedQuery<Restaurant> query = entityManager.createQuery(jpql.toString(), Restaurant.class);
+
+        parameters.forEach((key, value)-> query.setParameter(key,value));
+
+        return query.getResultList();
+
+//        return entityManager.createQuery(jpql.toString(), Restaurant.class)
+//                .setParameter("name", "%" + name + "%")
+//                .setParameter("initialDeliveryFee", initialDeliveryFee)
+//                .setParameter("finalDeliveryFee", finalDeliveryFee)
+//                .getResultList();
     }
 }
 
