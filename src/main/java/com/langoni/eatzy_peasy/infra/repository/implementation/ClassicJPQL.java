e729f0be4d2cfaf635542fd3ba27b27b93409014
Rafael Langoni <langoninaves@gmail.com>
2024-12-30 07:02:41 -0330

    Implementing Criteria to build queries

--- src/main/java/com/langoni/eatzy_peasy/infra/repository/implementation/ClassicJPQL.java
+++ src/main/java/com/langoni/eatzy_peasy/infra/repository/implementation/ClassicJPQL.java
@@ -4,32 +4,27 @@ import com.langoni.eatzy_peasy.domain.model.Restaurant;
 import com.langoni.eatzy_peasy.domain.repository.RestaurantRepositoryImplInterface;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 @Repository
-public class RestaurantRepositoryImpl implements RestaurantRepositoryImplInterface {
+public class ClassicJPQL implements RestaurantRepositoryImplInterface {
 
     @PersistenceContext
     private EntityManager entityManager;
 
     public List<Restaurant> listAllRestaurantCustomizedInterface(String name, BigDecimal initialDeliveryFee, BigDecimal finalDeliveryFee) {
 
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Restaurant> criteria = criteriaBuilder.createQuery(Restaurant.class);
+        var jpql = "select r from Restaurant r where r.name like :name " +
+                "and r.deliveryFee between :initialDeliveryFee and :finalDeliveryFee ";
 
-        criteria.from(Restaurant.class);
-
-        TypedQuery<Restaurant> query = entityManager.createQuery(criteria);
-        return query.getResultList();
-
+        return entityManager.createQuery(jpql.toString(), Restaurant.class)
+                .setParameter("name", "%" + name + "%")
+                .setParameter("initialDeliveryFee", initialDeliveryFee)
+                .setParameter("finalDeliveryFee", finalDeliveryFee)
+                .getResultList();
     }
 }
 
---
+++ src/main/java/com/langoni/eatzy_peasy/infra/repository/implementation/RepositoryWithDynamicParameters.java
@@ -0,0 +1,52 @@
+package com.langoni.eatzy_peasy.infra.repository.implementation;
+
+import com.langoni.eatzy_peasy.domain.model.Restaurant;
+import com.langoni.eatzy_peasy.domain.repository.RestaurantRepositoryImplInterface;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class RepositoryWithDynamicParameters implements RestaurantRepositoryImplInterface {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Restaurant> listAllRestaurantCustomizedInterface(String name, BigDecimal initialDeliveryFee, BigDecimal finalDeliveryFee) {
+
+
+        // Below we have some dynamic query, where I can set the parameters depending on it's filled or not
+        StringBuilder jpql = new StringBuilder();
+        jpql.append("select r from Restaurant r where 0=0 ");
+
+        var parameters = new HashMap<String, Object>();
+
+        if (StringUtils.hasLength(name)) {
+            jpql.append(" and r.name like :name ");
+            parameters.put("name", "%" + name + "%");
+        }
+
+        if (initialDeliveryFee != null) {
+            jpql.append(" and r.deliveryFee >= :initialFee");
+            parameters.put("initialFee", initialDeliveryFee);
+        }
+
+        if (finalDeliveryFee != null) {
+            jpql.append(" and r.deliveryFee <= :finalFee ");
+            parameters.put("finalFee", finalDeliveryFee);
+        }
+
+        TypedQuery<Restaurant> query = entityManager.createQuery(jpql.toString(), Restaurant.class);
+
+        parameters.forEach((key, value) -> query.setParameter(key, value));
+
+        return query.getResultList();
+
+    }
+}
